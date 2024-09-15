b5cb742c18b8d53db404eb22c8292dd5676a4faa
jckstllrd <jackstallard17@gmail.com>
2024-09-15 21:26:45 +0100

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, repetitions) {
+  let finalString = "";
+  if (repetitions < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < repetitions; i++) {
+    finalString += string;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
