5bc06d53443c75b438a30fea0fbc4c7dbb453199
jckstllrd <jackstallard17@gmail.com>
2024-09-15 21:33:23 +0100

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reversedString = "";
+  let stringLength = string.length;
+  for (let i = stringLength - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  console.log(reversedString);
 
+  return reversedString;
 };
 
 // Do not edit below this line
