fbb307bbb8cf444db0a319e970ec099a9a74792d
jckstllrd <jackstallard17@gmail.com>
2024-09-16 09:24:37 +0100

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  if (numOne < 0 || numTwo < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+    return "ERROR";
+  }
 
+  let count = 0;
+
+  if (numOne > numTwo) {
+    for (let i = numTwo; i <= numOne; i++) {
+      count += i;
+    }
+  } else if (numTwo > numOne) {
+    for (let i = numOne; i <= numTwo; i++) {
+      count += i;
+    }
+  }
+  return count;
 };
 
 // Do not edit below this line
