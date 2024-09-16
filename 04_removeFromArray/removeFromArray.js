8b85f55eb4097b3eb8c69ef56322f8ecf037af5f
jckstllrd <jackstallard17@gmail.com>
2024-09-16 08:36:43 +0100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+  newArray = [];
+  argsArray = [];
+
+  for (let i = 1; i < arguments.length; i++) {
+    argsArray.push(arguments[i]);
+  }
+
+  for (let x = 0; x < arguments.length; x++) {
+    while (array.includes(arguments[x], 0)) {
+      let index = array.indexOf(arguments[x]);
+
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
