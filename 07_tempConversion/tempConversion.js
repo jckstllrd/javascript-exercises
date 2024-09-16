550f22b5702c56082d1d3293b2b63fb4e55a5c00
jckstllrd <jackstallard17@gmail.com>
2024-09-16 09:41:40 +0100

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+  let celsius = ((farenheit - 32) * 5) / 9;
+  celsius = parseFloat(celsius.toFixed(1));
+  console.log(celsius);
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let farenheit = celsius * (9 / 5) + 32;
+  farenheit = parseFloat(farenheit.toFixed(1));
+  console.log(farenheit);
+
+  return farenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
