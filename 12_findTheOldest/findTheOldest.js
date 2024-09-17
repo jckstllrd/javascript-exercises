354e3f23543a3b8a57139cf6a87437ce326c8a4a
jckstllrd <jackstallard17@gmail.com>
2024-09-17 18:44:37 +0100

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let oldestPerson;
+  let greatestAge = 0;
 
+  people.forEach((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = 2024;
+      console.log("undefined now is " + person.yearOfDeath);
+    }
+
+    let age = person.yearOfDeath - person.yearOfBirth;
+    console.log("next age is" + age);
+    console.log(greatestAge);
+
+    if (age > greatestAge) {
+      console.log("here");
+
+      oldestPerson = person;
+      greatestAge = age;
+      console.log(age);
+      console.log("new oldest is " + person.name);
+    }
+  });
+
+  console.log(oldestPerson.name);
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
