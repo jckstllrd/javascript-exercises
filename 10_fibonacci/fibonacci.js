1f778213cb36297f28c13f818e7da55f2cc93197
jckstllrd <jackstallard17@gmail.com>
2024-09-17 18:01:31 +0100

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  } else if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else if (n == 2) {
+    return 1;
+  }
 
+  let fib = [1, 1];
+
+  for (let i = 1; i < n; i++) {
+    let nextFib = fib[i - 1] + fib[i];
+    fib.push(nextFib);
+  }
+
+  console.log(fib);
+
+  return fib[fib.length - 2];
 };
 
 // Do not edit below this line
