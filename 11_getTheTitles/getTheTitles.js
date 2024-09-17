f59cf285feb012ab0f961bb4f6a0ea02381089d8
jckstllrd <jackstallard17@gmail.com>
2024-09-17 18:04:59 +0100

    Complete 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  let bookList = [];
 
+  books.forEach((book) => {
+    bookList.push(book.title);
+  });
+  return bookList;
 };
 
 // Do not edit below this line
