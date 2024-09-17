36278851fed3a29714e8c8c9721b3b2d5dd28c89
jckstllrd <jackstallard17@gmail.com>
2024-09-17 17:47:34 +0100

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let cleanWord = stripWord(word);
 
+  let isPalindrome = false;
+  let finalWord = "";
+
+  console.log(cleanWord);
+
+  for (let i = cleanWord.length - 1; i >= 0; i--) {
+    finalWord += cleanWord[i];
+  }
+  if (finalWord === cleanWord) {
+    isPalindrome = true;
+  }
+  console.log(finalWord);
+
+  return isPalindrome;
+};
+
+const stripWord = function (word) {
+  let punctuation = /[^\w\s\']|_/g;
+
+  let newWord = word.replace(punctuation, "").replace(/ /g, "");
+  newWord = newWord.toLowerCase();
+  console.log("stipped word is " + newWord);
+
+  return newWord;
 };
 
 // Do not edit below this line
